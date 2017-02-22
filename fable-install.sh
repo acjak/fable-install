8bcc85380ddd25075ccd999fefe7aba19942d03b
andcj <sterrior@gmail.com>
2017-02-22 11:12:49 +0100

    First commit.

---
+++ fable-install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+url=https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh
+curl $url -o miniconda.sh
+chmod +x miniconda.sh
+./miniconda.sh -b
+echo 'export PATH=$HOME/miniconda2/bin:$PATH' >> ~/.bashrc
+conda update --yes conda
+conda config --add channels http://conda.binstar.org/fable
+conda install 3dxrd-scripts
